e5d30b721b2439bcac7d12a588c90e5f195d6ca2
Renita <ira3624@icloud.com>
2023-01-21 22:44:45 +0200

    hw-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartAnimeColor);
+stopBtn.addEventListener('click', onStopAnimeColor);
+
+let idStart;
+startBtn.disabled = false;
+
+function onStartAnimeColor(evt) {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  document.body.style.backgroundColor = getRandomHexColor();
+  idStart = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopAnimeColor() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(idStart);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
