58a2b5f48595bbfae5c72a64584546d36aa0723f
Renita <ira3624@icloud.com>
2023-01-23 02:19:11 +0200

    hw-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const firstDelay = document.querySelector("input[name='delay']");
+const delayStep = document.querySelector("input[name='step']");
+const amount = document.querySelector("input[name='amount']");
+
+form.addEventListener('submit', onResultMessage);
+
+function onResultMessage(evt) {
+  evt.preventDefault();
+  let delay = Number(firstDelay.value);
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += Number(delayStep.value);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      setTimeout(() => resolve({ position, delay }), delay);
+    } else {
+      setTimeout(() => reject({ position, delay }), delay);
+    }
+  });
 }
